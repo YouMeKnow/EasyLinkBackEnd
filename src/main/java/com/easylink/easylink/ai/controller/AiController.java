5de673a26b290cbd5127f30fc3703e0ae839b209
Andrey <andrew.saltul@gmail.com>
2026-03-09 10:41:11 -0400

    Ai integration structure

---
+++ src/main/java/com/easylink/easylink/ai/controller/AiController.java
@@ -0,0 +1,14 @@
+package com.easylink.easylink.ai.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v3/ai")
+@RequiredArgsConstructor
+public class AiController {
+
+
+
+}
---
+++ src/main/java/com/easylink/easylink/ai/service/AiChatService.java
@@ -0,0 +1,11 @@
+package com.easylink.easylink.ai.service;
+
+import com.google.genai.Client;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AiChatService {
+
+
+
+}
